5e104d148ad6611eebb73ffddcd81276295650fe
Your Name <you@example.com>
2022-08-28 14:26:16 -0400

    update

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,17 @@ import Sigma from 'sigma';
 //with node_modules we dont need to write out the path, just the name of the package
 
 import './styles/main.scss'
-import Fig3 from './assets/smiley.svg';
+import Fig3 from './assets/smiley.svg'; //Add svg type to custom.d.ts for this import to work
 
-import hello from './mystuff';
-//Notice our own js code(not from node_modules) uses relative path and we can omit the `.js` 
+import hello from './mystuff'; //shorthand for mystuff.ts
+//Our own(not from node_modules) js code uses relative path meaning 
+//we have to write `./someJslib` instead of just `someJslib`
 
 //Notice even though the function name is `sayhello` from `export default sayhello;` in `mystuff.js`
-  //we are allowed to rename the import as `hello`
+//we are allowed to rename the import as `hello`
 
+
+import data from "./assets/data.json"; //`Add "resolveJsonModule": true` in tsconfig.json for this import to work
 
 
 
@@ -21,6 +24,9 @@ somePicHTML.src = Fig3;
 const randNum = () => {
   return Math.floor(Math.random() * (10-0+1))
 }
+
+
+// First graph - adding node manually
 
 const graph = new Graph();
 
@@ -35,3 +41,19 @@ const settingsSigma = {
 
 const renderer = new Sigma(graph,container!,settingsSigma);
 //the ! in `container!` asserts that `container` is non-null (this stops the IDE warnings)
+
+
+//2nd Graph - importing nodes with json
+
+const graph2 = new Graph();
+
+
+
+graph2.import(data)
+
+const container2 : (HTMLElement | null) = document.getElementById("sigma-container2");
+const settingsSigma2 = {
+    labelRenderedSizeThreshold: 1,
+  }
+
+const renderer2 = new Sigma(graph2,container2!,settingsSigma2);
